f3c4db94fd5c5f8c040afec4f76784f882fee351
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2022-07-15 14:27:21 +0200

    ok test

---
+++ tt_lib_dao.py
@@ -0,0 +1,4 @@
+from dao.UserDao import UserDao as UserDao
+from dao.UserMemoDao import UserDao as UserMemoDao
+from dao.UserSQLDao import UserDao as UserSQLDao
+from dao.UserMongoDao import UserDao as UserMongoDao
