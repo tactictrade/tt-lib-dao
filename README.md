d0b299baacbd6695cb392da1b92736ada6747ae7
Cecilio Cannavacciuolo <c.cannavacciuolo@anoki.it>
2022-07-15 11:23:18 +0200

    add dao

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Welcome to library tt-lib-dao
 
-## Install library in localhost 
+## Install library in localhost
+
+Install in local 
 
     pip install -e .
 
+Check in local
 
-
+    python -c "import tt_lib_dao; tt_lib_dao.UserDao"
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='dao',
+    name='tt_lib_dao',
     version="0.0.1",
-    url='https://github.com/tactictrade/tt-lib-dao.git',
+    url='https://github.com/tactictrade/tt-lib-tt_lib_dao.git',
     author='CannavIT',
     author_email='cecilio.cannav@gmail.com',
-    py_modules=['dao'],
-    description="Dao Library for TacticTrade",
+    py_modules=['tt_lib_dao'],
+    description="tt_lib_dao Library for TacticTrade",
     entry_points={
     'console_scripts': [
-        'add=dao:cmd_add',
+        'add=tt_lib_dao:cmd_add',
     ],
 },
 )
